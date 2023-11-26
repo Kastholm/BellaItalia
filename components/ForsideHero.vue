938d32951c186a274c885702e24be962bcb23dde
Anton B. Christensen <webbermanden@gmail.com>
2023-11-27 00:39:27 +0100

    Forside færdig

--- components/ForsideHero.vue
+++ components/ForsideHero.vue
@@ -8,25 +8,24 @@
         class="relative isolate overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20"
       >
         <img
-          src="../assets/img/forside/bellaForside.jpg"
+          src="https://i.ibb.co/VmZX39b/a9ezar46fbxjvuyd8r2z-1.webp"
           alt=""
-          class="absolute inset-0 -z-10 h-full w-full object-cover"
+          class="absolute inset-0 -z-20 h-full w-full object-cover"
         />
-        
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto max-w-2xl py-10">
+          <div class="mx-auto max-w-2xl py-4">
+           <div class="absolute bg-black inset-0 bg-opacity-50 -z-10"></div>
             <div class="text-center">
               <h1
-                class="text-4xl text-[#009246] font-bold tracking-tight text-white sm:text-6xl"
+                class="text-4xl text-[#009246] font-bold tracking-tight text-[#009246] sm:text-6xl"
               >
                 Traditionel Italiensk Cuisine siden 1989
+                <div class="after ml-64 mt-3 z-10"></div>
               </h1>
-              <p class="mt-6 mb-32 text-lg leading-8 text-gray-300">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+              <p class="mt-6 mb-32 text-lg leading-8 text-white md:w-[700px]">
+                Hos Bella Italia har vi siden 1989 skabt autentisk Italiesk Cuisine i hjertet af Kolding By. Kom ind og spis Alla Carte eller bestil Take-away.
               </p>
-              <button href="#" class="hover:scale-105 transition text-xl absolute top-40 text-black h-14 w-32 bg-white text-white inset-0 m-auto rounded">
+              <button href="#" class="hover:scale-105 transition font-bold text-lg absolute top-40 text-[#1c1c1c] h-14 w-32 bg-[#eeecec] text-black inset-0 m-auto rounded">
              Kontakt os
             </button>
             </div>
@@ -38,19 +37,21 @@
     
      
     <div class="flex block mt-1 gap-1"> 
-      
-          <div alt="" class="w-[50%] max-w-[50%] h-60 bgimg relative">
-            
+          <div alt="" class="w-[50%] max-w-[50%] h-60 bgimg relative z-0 bg-bottom">
+            <div class="absolute bg-black inset-0 bg-opacity-50 -z-10"></div>
               <h2 class="text-3xl text-white left-[25px] h-4 w-60 bottom-28 absolute inset-0 m-auto">VORES UDVALG</h2>
-              <button class=" hover:scale-105 transition text-xl h-14 w-32 bg-[#009246] text-white rounded absolute inset-0 m-auto top-10">
+              <div class="after mt-[95px] ml-[274px] mt-3 z-10"></div>
+              <button class=" hover:scale-105 transition font-bold text-lg h-14 w-32 bg-[#eeecec] text-[#1c1c1c] rounded absolute inset-0 m-auto top-10">
               Menu
               </button>
           </div>
 
         
-          <div alt="" class="w-[50%] max-w-[50%] h-60 bgimg2 relative" >
+          <div alt="" class="w-[50%] max-w-[50%] h-60 bgimg2 relative z-0" >
+            <div class="absolute bg-black inset-0 bg-opacity-50 -z-10"></div>
+            <div class="after mt-[95px] ml-[273px] mt-3 z-10"></div>
             <h2 class="text-3xl text-white left-[85px] h-4 w-60 bottom-28 absolute inset-0 m-auto">TAKE AWAY</h2>
-            <button class="hover:scale-105 transition text-xl absolute h-14 w-32 bg-[#ce2b37] text-white inset-0 m-auto top-10 rounded">
+            <button class="hover:scale-105 transition font-bold text-lg absolute h-14 w-32 bg-[#eeecec] text-[#1c1c1c] inset-0 m-auto top-10 rounded">
              Bestil
             </button>
           </div>
@@ -59,8 +60,9 @@
       <div class="mt-24">
         <div class="mx-auto max-w-7xl px-6 lg:px-8 mb-32">
           <div class="mx-auto max-w-2xl sm:text-center">
-            <h2 class="text-base max-w-[150px] m-auto font-semibold leading-7 text-[#42934d] after">
-              Ægte Italiensk mad
+            <h2 class="text-base max-w-[160px] m-auto font-semibold leading-7 text-[#42934d]">
+              Ægte Italiensk Cuisine
+              <div class="after ml-1 mt-1"></div>
             </h2>
             <p
               class="mt-10 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
@@ -94,12 +96,12 @@ background: linear-gradient(to right, #009246 0%, #009246 33.33%, #cecdcd 33.33%
 }
 
 .bgimg {
-  background-image: url("https://cdn.sanity.io/images/drjvsy8w/production/d2215c32d53dfb6c9538af1ae8b7cd2d69df1a3b-2048x1365.jpg");
+  background-image: url("https://i.ibb.co/QdpBdgd/recipe-pappardelle-1.webp");
   background-size: cover;
 }
 
 .bgimg2 {
-  background-image: url("https://cdn.sanity.io/images/drjvsy8w/production/d2215c32d53dfb6c9538af1ae8b7cd2d69df1a3b-2048x1365.jpg");
+  background-image: url("https://i.ibb.co/G9NGdLz/7f71fcc474fcf74c620f23d6c559fc43-1.webp");
   background-size: cover;
 }
 </style>

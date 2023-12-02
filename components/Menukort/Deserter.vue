b58168dfe81df842d3d5e152c7b0c74a98c76fb8
Marc <marc@aguardio.com>
2023-12-02 12:08:14 +0100

    yes

---
+++ components/Menukort/Deserter.vue
@@ -0,0 +1,67 @@
+<template>
+  <div v-if="!data" class="flex justify-center items-center">
+    <div class="loader"></div>
+  </div>
+  <div v-else class="shadow-md rounded-lg overflow-hidden">
+    <div
+      v-for="menuPunkt in data"
+      :key="menuPunkt.title"
+      class="menu-section max-w-8xl mx-auto p-4"
+    >
+      <h1
+        class="text-4xl text-center font-bold uppercase text-gray-800 font-header after mb-12"
+      >
+        {{ menuPunkt.title }}
+      </h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div
+          class="menu-item flex min-h-[8em]"
+          v-for="item in menuPunkt.sections"
+          :key="item._key"
+        >
+          <div
+            class="menu-number flex items-center justify-center bg-gray-200 text-gray-500 font-bold text-3xl"
+            style="flex: none; width: 100px; height: 100px"
+          >
+            {{ item.number }}
+          </div>
+          <div class="menu-text-content flex flex-col justify-between p-4">
+            <div>
+              <div class="menu-name text-xl font-bold">{{ item.name }}</div>
+              <div class="menu-description text-gray-600">
+                {{ item.description }}
+              </div>
+            </div>
+            <div class="menu-price text-green-900 text-[1.3rem] font-bold">
+              {{ item.price }} kr.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+const query = groq`*[_id == "04d4ca6d-3278-4b7a-a84b-3b405090d78f"] {
+  title,
+  sections [] {
+    _key,
+    _type,
+    description,
+    name,
+    number,
+    price,
+    takeawayprice
+  }
+}`;
+
+const sanity = useSanity();
+const { data } = useSanityQuery(query);
+
+console.log(data);
+</script>
+
+<style>
+</style>
--- components/Omos.vue
+++ components/Omos.vue
@@ -1,34 +1,36 @@
 <template>
-  <div v-for="omos in data.omosinfo" :key="omos._id" class="mt-24">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 mb-32">
-      <div class="mx-auto max-w-2xl sm:text-center">
-        <h2
-          class="text-base max-w-[160px] m-auto font-semibold leading-7 text-[#42934d]"
-        >
-          Ægte Italiensk Cuisine
-          <div class="after ml-1 mt-1"></div>
-        </h2>
-        <p
-          class="mt-10 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl font-header"
-        >
-          {{ omos.title }}
-        </p>
-
-        <p class="mt-6 text-lg leading-8 text-gray-600">
-          <SanityBlocks
-            :blocks="omos.descriptiondk"
-            :serializers="serializers"
-          />
-        </p>
-        <br /><br />
-        <div v-if="omos.descriptionen">
-          <h2 class="text-2xl font-header">English :</h2>
-          <p class="text-lg leading-8 text-gray-600">
+  <div v-if="data">
+    <div v-for="omos in data.omosinfo" :key="omos._id" class="mt-24">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8 mb-32">
+        <div class="mx-auto max-w-2xl sm:text-center">
+          <h2
+            class="text-base max-w-[160px] m-auto font-semibold leading-7 text-[#42934d]"
+          >
+            Ægte Italiensk Cuisine
+            <div class="after ml-1 mt-1"></div>
+          </h2>
+          <p
+            class="mt-10 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl font-header"
+          >
+            {{ omos.title }}
+          </p>
+  
+          <p class="mt-6 text-lg leading-8 text-gray-600">
             <SanityBlocks
-              :blocks="omos.descriptionen"
+              :blocks="omos.descriptiondk"
               :serializers="serializers"
             />
           </p>
+          <br /><br />
+          <div v-if="omos.descriptionen">
+            <h2 class="text-2xl font-header">English :</h2>
+            <p class="text-lg leading-8 text-gray-600">
+              <SanityBlocks
+                :blocks="omos.descriptionen"
+                :serializers="serializers"
+              />
+            </p>
+          </div>
         </div>
       </div>
     </div>
